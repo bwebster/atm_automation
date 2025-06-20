f8d3acb8651d361de4a0a393a4408cf349208c31
Burke Webster <burke.webster@gmail.com>
2025-06-20 17:33:15 -0500

    Prototyping WLED + sound

--- esp32_to_esp32_wled/esp32_to_esp32_wled.ino
+++ esp32_to_esp32_wled/esp32_to_esp32_wled.ino
@@ -7,7 +7,18 @@
 */
 
 #include <Arduino.h>
+#include <DYPlayerArduino.h> // External: 
 #include <ArduinoJson.h> // External: https://github.com/bblanchon/ArduinoJson v7.3.0
+
+// Pins used to communicate with Arduino (scanner)
+constexpr uint8_t TX_PIN = 19;
+constexpr uint8_t RX_PIN = 18;
+
+// Pins used to communicate with mp3 player
+HardwareSerial MP3Serial(2);
+constexpr uint8_t MP3_TX_PIN = 33;
+constexpr uint8_t MP3_RX_PIN = 25;
+constexpr uint32_t MP3_BAUD = 9600;
 
 // ── UART to WLED ──────────────────────────────────────────────
 HardwareSerial WLED(1);             // UART1  (RX16 / TX17)
@@ -57,31 +68,87 @@ void changeColor(uint8_t r, uint8_t g, uint8_t b) {
   sendJson(j);
 }
 
-// ── Demo sequence ────────────────────────────────────────────
+void turnOnPreset(uint8_t id)
+{
+  StaticJsonDocument<64> j;
+  j["on"] = true;
+  j["ps"] = id;
+  sendJson(j);
+}
+
+DY::Player player(&MP3Serial);
+int track = 1;
+int numTracks = 3;
+bool last = LOW;
+bool playing = false;
+
 void setup() {
   Serial.begin(115200);
+
+  Serial.println("Setting up pins");
+  pinMode(TX_PIN, OUTPUT);
+  pinMode(RX_PIN, INPUT_PULLDOWN);
+  digitalWrite(TX_PIN, HIGH);
+  
+  Serial.println("Setting up WLED");
   WLED.begin(WLED_BAUD, SERIAL_8N1, WLED_RX, WLED_TX);
+  delay(200);
+
+  Serial.println("Testing LEDs");
+  turnOff();
+  delay(100);
+  turnOnPreset(1);
+  delay(1000);
+  turnOnPreset(2);
+  delay(1000);
+  turnOff();
+  delay(100);
+
+  Serial.println("Initializing mp3 module");
+  MP3Serial.begin(MP3_BAUD, SERIAL_8N1, MP3_RX_PIN, MP3_TX_PIN);
+  delay(800);
+  player.begin();
+  player.setVolume(25);        // 0…30
 }
 
 void loop() {
- Serial.println(F("→ turnOn(Bouncing Balls)"));
-  changeColor(0, 255, 0); // Green
-  turnOn(BOUNCING_BALLS_ID);
-  delay(10'000);
+  bool cur = digitalRead(RX_PIN);
+  if (last == LOW && cur == HIGH) {
+    Serial.println("Received HIGH from arduino");
 
- Serial.println(F("→ turnOn(Chase)"));
- changeColor(0, 0, 255); // Blue
-  turnOn(CHASE_ID);
-  delay(10'000);
+    turnOnPreset(1);
+    delay(100);
+    player.playSpecified(1);
+    delay(5000);
+    turnOff();
 
-  Serial.println(F("→ changeEffect(Solid)"));
-  changeColor(255, 255, 255); // White
-  changeEffect(SOLID_ID);
-  delay(5000);
+    Serial.println("Sending done signal (HIGH)");
+    digitalWrite(TX_PIN, LOW);
+    delay(200);
+    digitalWrite(TX_PIN, HIGH);
 
-  Serial.println(F("→ turnOff()"));
-  turnOff();
+    Serial.println("Done");
+  }
+  last = cur;
 
-  Serial.println(F("Sleep"));
-  delay(5000);
+//  Serial.println(F("→ turnOn(Bouncing Balls)"));
+//   changeColor(0, 255, 0); // Green
+//   turnOn(BOUNCING_BALLS_ID);
+//   delay(10'000);
+
+//  Serial.println(F("→ turnOn(Chase)"));
+//  changeColor(0, 0, 255); // Blue
+//   turnOn(CHASE_ID);
+//   delay(10'000);
+
+//   Serial.println(F("→ changeEffect(Solid)"));
+//   changeColor(255, 255, 255); // White
+//   changeEffect(SOLID_ID);
+//   delay(5000);
+
+//   Serial.println(F("→ turnOff()"));
+//   turnOff();
+
+//   Serial.println(F("Sleep"));
+//   delay(5000);
 }
