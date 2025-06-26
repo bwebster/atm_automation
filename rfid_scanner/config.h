08d94463bc381b5f51c2ac0386bc1c2fb4e90f1b
Burke Webster <burke.webster@gmail.com>
2025-06-26 08:55:27 -0500

    Reset config

--- rfid_scanner/config.h
+++ rfid_scanner/config.h
@@ -47,7 +47,7 @@ NoAutomation automation;
 #define RECENT_SCAN_HISTORY_SIZE 1
 
 // Location number to send after successful scan
-#define LOCATION 1
+#define LOCATION 0
 
 // Configure the IP address and port for the server software.
 // const char *server = "192.168.5.229";
